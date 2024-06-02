1bf2e7469bcc90021bbc50782626612f8bd783dd
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2024-06-02 16:57:20 +0500

    feat: Add Kafka producer and session dependencies
    The code changes add a new file `deps.py` which includes functions for getting a Kafka producer and a database session. These dependencies will be used in other parts of the application. This commit is part of the ongoing development of the AI Mart platform.

--- product-service/app/crud/product_crud.py
+++ product-service/app/crud/product_crud.py
@@ -1,5 +1,6 @@
+from fastapi import HTTPException
 from sqlmodel import Session, select
-from app.models.product_model import Product
+from app.models.product_model import Product, ProductUpdate
 
 # Add a New Product to the Database
 def add_new_product(product_data: Product, session: Session):
@@ -13,3 +14,34 @@ def add_new_product(product_data: Product, session: Session):
 def get_all_products(session: Session):
     all_products = session.exec(select(Product)).all()
     return all_products
+
+# Get a Product by ID
+def get_product_by_id(product_id: int, session: Session):
+    product = session.exec(select(Product).where(Product.id == product_id)).one_or_none()
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
+
+# Delete Product by ID
+def delete_product_by_id(product_id: int, session: Session):
+    # Step 1: Get the Product by ID
+    product = session.exec(select(Product).where(Product.id == product_id)).one_or_none()
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    # Step 2: Delete the Product
+    session.delete(product)
+    session.commit()
+    return {"message": "Product Deleted Successfully"}
+
+# Update Product by ID
+def update_product_by_id(product_id: int, to_update_product_data:ProductUpdate, session: Session):
+    # Step 1: Get the Product by ID
+    product = session.exec(select(Product).where(Product.id == product_id)).one_or_none()
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    # Step 2: Update the Product
+    hero_data = to_update_product_data.model_dump(exclude_unset=True)
+    product.sqlmodel_update(hero_data)
+    session.add(product)
+    session.commit()
+    return product
--- product-service/app/main.py
+++ product-service/app/main.py
@@ -1,19 +1,18 @@
 # main.py
 from contextlib import asynccontextmanager
-from typing import Union, Optional, Annotated
-from sqlmodel import Field, Session, SQLModel, create_engine, select, Sequence
-from fastapi import FastAPI, Depends
+from typing import Annotated
+from sqlmodel import Session, SQLModel
+from fastapi import FastAPI, Depends, HTTPException
 from typing import AsyncGenerator
 from aiokafka import AIOKafkaProducer, AIOKafkaConsumer
 import asyncio
 import json
-from app import todo_pb2
 
 from app import settings
 from app.db_engine import engine
-from app.models.product_model import Product
-from app.crud.product_crud import add_new_product, get_all_products
-
+from app.models.product_model import Product, ProductUpdate
+from app.crud.product_crud import add_new_product, get_all_products, get_product_by_id, delete_product_by_id, update_product_by_id
+from app.deps import get_session, get_kafka_producer
 
 def create_db_and_tables() -> None:
     SQLModel.metadata.create_all(engine)
@@ -71,29 +70,16 @@ app = FastAPI(
 )
 
 
-def get_session():
-    with Session(engine) as session:
-        yield session
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "Product Service"}
 
-# Kafka Producer as a dependency
-
-
-async def get_kafka_producer():
-    producer = AIOKafkaProducer(bootstrap_servers='broker:19092')
-    await producer.start()
-    try:
-        yield producer
-    finally:
-        await producer.stop()
-
 
 @app.post("/manage-products/", response_model=Product)
 async def create_new_product(product: Product, session: Annotated[Session, Depends(get_session)], producer: Annotated[AIOKafkaProducer, Depends(get_kafka_producer)]):
+    """ Create a new product and send it to Kafka"""
+    
     product_dict = {field: getattr(product, field) for field in product.dict()}
     product_json = json.dumps(product_dict).encode("utf-8")
     print("product_JSON:", product_json)
@@ -102,8 +88,39 @@ async def create_new_product(product: Product, session: Annotated[Session, Depen
     # new_product = add_new_product(product, session)
     return product
 
+@app.get("/manage-products/all", response_model=list[Product])
+def call_all_products(session: Annotated[Session, Depends(get_session)]):
+    """ Get all products from the database"""
+    return get_all_products(session)
 
-# @app.get("/todos/", response_model=list[Todo])
-# def read_todos(session: Annotated[Session, Depends(get_session)]):
-#     todos = session.exec(select(Todo)).all()
-#     return todos
+@app.get("/manage-products/{product_id}", response_model=Product)
+def get_single_product(product_id: int, session: Annotated[Session, Depends(get_session)]):
+    """ Get a single product by ID"""
+    try:
+        return get_product_by_id(product_id=product_id, session=session)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.delete("/manage-products/{product_id}", response_model=dict)
+def delete_single_product(product_id: int, session: Annotated[Session, Depends(get_session)]):
+    
+    
+    """ Delete a single product by ID"""
+    try:
+        return delete_product_by_id(product_id=product_id, session=session)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.patch("/manage-products/{product_id}", response_model=Product)
+def update_single_product(product_id: int, product: ProductUpdate, session: Annotated[Session, Depends(get_session)]):
+    """ Update a single product by ID"""
+    try:
+        return update_product_by_id(product_id=product_id, to_update_product_data=product, session=session)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
